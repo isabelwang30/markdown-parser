960182ea5a2ecc4ac4737606fd21a66f525a2b69
isabelwang30 <103291789+isabelwang30@users.noreply.github.com>
2022-04-15 16:58:52 -0700

    fixed the problem with the image link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,13 +16,15 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (markdown.charAt(openBracket - 1) != '!') {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
     }
 
-
+    
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
