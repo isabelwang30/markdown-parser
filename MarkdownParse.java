bec17c631caa29a7c7376c5ba7e762bd77ea9ce7
isabelwang30 <103291789+isabelwang30@users.noreply.github.com>
2022-04-15 17:13:51 -0700

    Update MarkdownParse.java
    fixed the problem with empty lines at the end of the file

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,13 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            
+            if (openBracket == -1 || closeBracket == -1 || openParen == -1 
+                || closeParen == -1) 
+            {
+                break;
+            }
+
             if (markdown.charAt(openBracket - 1) != '!') {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
@@ -24,7 +31,7 @@ public class MarkdownParse {
         return toReturn;
     }
 
-    
+
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
