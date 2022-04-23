da9b301f0c59a37a3f66ccb8cc59d048abdf7c34
isabelwang30 <103291789+isabelwang30@users.noreply.github.com>
2022-04-22 17:17:14 -0700

    Update MarkdownParseTest.java
    added JUnit test for getLinks() on test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,24 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.io.IOException;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinksTest() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        List<String> links = MarkdownParse.getLinks(content);
+        assertEquals(List.of("https://something.com", "some-thing.html"),
+            links);
     }
 }
